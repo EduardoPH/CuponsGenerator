091926631115744d564bfe812d8bf6d1a5c658e3
EduardoPH <dudu19424irma@gmail.com>
2023-05-04 22:23:06 -0300

    Change title

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="w-auto flex py-4 px-8">
-      <span className="text-2xl text-primary font-bold">CuponsGenerator<span className="text-4xl text-tertiary">.</span>U</span>
+      <span className="text-2xl text-primary font-bold">McDonaldsGenerator<span className="text-4xl text-tertiary">.</span>U</span>
     </header>
   );
 }
